4ac544c308648045804a780cdb414ba841f36023
Thomas Krijnen <t.krijnen@gmail.com>
2019-09-20 12:37:16 +0200

    support for lines in prepared buffer format

--- server/prepared_buffer.py
+++ server/prepared_buffer.py
@@ -13,7 +13,7 @@ def normalToOct(normal):
     return numpy.int8(p * 127.)
 
 def create(ifn, ofn):
-    indices, vertices, offsets = [], [], []
+    indices, line_indices, vertices, offsets, line_offsets = [], [], [], [], []
 
     for l in open(ifn):
         args = l.strip().split(" ")
@@ -23,14 +23,25 @@ def create(ifn, ofn):
         if a == "v":
             vertices.extend(map(float, bs))
         elif a == "f":
+            bs = list(map(lambda s: s.split("/")[0], bs))
+            assert len(bs) == 3
             indices.extend(map(int, bs))
-        elif a == "g":
+        elif a == "l":
+            bs = list(map(lambda s: s.split("/")[0], bs))
+            assert len(bs) == 2
+            line_indices.extend(map(int, bs))
+        elif a in "og":
             offsets.append([len(indices), len(vertices)])
+            line_offsets.append([len(line_indices), len(vertices)])
             
     indices_np = numpy.array(indices, dtype=numpy.uint32).reshape((-1, 3)) - 1
+    line_indices_np = numpy.array(line_indices, dtype=numpy.uint32).reshape((-1, 2)) - 1
     vertices_np = numpy.array(vertices, dtype=numpy.float32).reshape((-1, 3))
-    normals = numpy.empty(vertices_np.shape, dtype=numpy.float32)
-
+    
+    # initialize to (0,0,1) in case we fail to calculate normals (e.g. in case of line vertices)
+    normals = numpy.zeros(vertices_np.shape, dtype=numpy.float32)
+    normals[:,2] = 1.
+    
     for idxs in indices_np:
         tri_vs = vertices_np[idxs]
         e0, e1 = tri_vs[1:] - tri_vs[0]
@@ -61,6 +72,7 @@ def create(ifn, ofn):
     colorsIndex    : int
 
     indices        : array<int, totalNrIndices>
+    lineIndices    : array<int, nrLineIndices>
 
     objects : array<struct, nrObjects>
         oid           : long
@@ -84,11 +96,12 @@ def create(ifn, ofn):
     """
 
     with open(ofn, "wb") as f:
-        numpy.array([len(offsets), indices_np.size, 0, vertices_np.size, normals.size, 0], dtype=numpy.int32).tofile(f)
+        numpy.array([len(offsets), indices_np.size * 3, line_indices_np.size * 2, vertices_np.size, normals.size, 0], dtype=numpy.int32).tofile(f)
         # alignment not necessary anymore
         # numpy.array([0], dtype=numpy.int32).tofile(f)
-        numpy.array([len(offsets), indices_np.size, 0, vertices_np.size, normals.size, 0], dtype=numpy.int32).tofile(f)
+        numpy.array([len(offsets), indices_np.size * 3, line_indices_np.size * 2, vertices_np.size, normals.size, 0], dtype=numpy.int32).tofile(f)
         indices_np.tofile(f)
+        line_indices_np.tofile(f)
         for i, (off, voff) in enumerate(offsets):
             numpy.array([0xffff + i], dtype=numpy.int64).tofile(f)
             try:
@@ -98,7 +111,12 @@ def create(ifn, ofn):
                 vnext = vertices_np.size
             off //= 3
             next //= 3
-            numpy.array([off, 0, next - off, 0, vnext - voff, indices_np[off:next].min(), indices_np[off:next].max()], dtype=numpy.int32).tofile(f)
+            
+            try:
+                min_index, max_index = indices_np[off:next].min(), indices_np[off:next].max()
+            except: min_index, max_index = -1, -1
+            
+            numpy.array([off, 0, next - off, 0, vnext - voff, min_index, max_index], dtype=numpy.int32).tofile(f)
             numpy.array([0.], dtype=numpy.float32).tofile(f)
             numpy.array([1, (vnext - voff) // 3 * 4, 0xff0000ff], dtype=numpy.uint32).tofile(f)
         
@@ -110,3 +128,8 @@ def create(ifn, ofn):
 
         for n in normals.reshape((-1, 3)):
             normalToOct(n).tofile(f)
+
+if __name__ == "__main__":
+    import sys
+    ifn, ofn = sys.argv[1:]
+    create(ifn, ofn)
