1a625d689e5857298e22d0297de77b02f25d68fb
Thomas Krijnen <t.krijnen@gmail.com>
2019-12-18 13:25:14 +0100

    Submodule

--- server/run_debug.bat
+++ server/run_debug.bat
@@ -1,3 +1,4 @@
 set FLASK_APP=main.py
 set FLASK_DEBUG=1 
-python -m flask run
+set VOXEC_EXE=C:\Users\tkrij\Documents\AECgeeks\projects\tno\voxel\build\RelWithDebInfo\voxec.exe
+C:\Users\tkrij\Anaconda3\python.exe -m flask run
